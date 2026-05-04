2be9830d1c526683c00aa7fcc4587d12aa90f34d
NTM-n270 <gpuminer02@gmail.com>
2026-05-03 22:31:25 -0700

    Add heat function to LeafiaRodItem instances

--- src/main/java/com/leafia/contents/AddonItems.java
+++ src/main/java/com/leafia/contents/AddonItems.java
@@ -124,7 +124,8 @@ public class AddonItems {
                 .setReactivity(0.4)
                 .setDecayProduct("depleteduranium")
                 .addRad(ItemRads.uraniumFuel)
-                .setCreativeTab(MainRegistry.controlTab);
+                .setCreativeTab(MainRegistry.controlTab)
+		        .setHeatFunction((0.00*8)^0.56*3);
 		public static final Item
                 leafRodMEAT210
                 = new LeafiaRodItem("Moderately-Enriched Astatine-210",160000, 80000)
@@ -132,6 +133,7 @@ public class AddonItems {
                 .setReactivity(0.8)
                 .setDecayProduct("depleteduranium")
                 .addRad(ItemRads.uraniumFuel)
+		        .setHeatFunction((0.00*8)^0.56*3)
                 .setCreativeTab(MainRegistry.controlTab);
 		public static final Item
                 leafRodHEAT210
@@ -140,6 +142,7 @@ public class AddonItems {
                 .setReactivity(1.6)
                 .setDecayProduct("depleteduranium")
                 .addRad(ItemRads.uraniumFuel)
+		        .setHeatFunction((0.00*8)^0.56*3)
                 .setCreativeTab(MainRegistry.controlTab);
 		public static final Item
 				leafRodHEU233
